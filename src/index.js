6c720af3fc60e99c3babff905550f45adcb05d8a
takuyatechexpert <lp4546.sg.abc@gmail.com>
2020-03-16 17:48:58 +0900

    implement component with connect function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import './index.css';
-import reducer from './reducers'
+import reducer from './reducers';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
@@ -11,9 +12,10 @@ const store = createStore(reducer)
 
 ReactDOM.render(
   <Provider store={store}>
-  <App />
+    <App />
   </Provider>,
-  document.getElementById('root'));
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
