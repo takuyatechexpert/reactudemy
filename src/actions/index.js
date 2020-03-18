635af6c0084e342a5a2292d84f59097dee23c740
takuyatechexpert <lp4546.sg.abc@gmail.com>
2020-03-18 20:59:45 +0900

    implement event new component

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 // リデューサーで再利用するらしい 再利用する場合はexportで記述する
 export const READ_EVENTS = 'READ_EVENTS'
+export const CREATE_EVENT = 'CREATE_EVENT'
+
 
 const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1'
 const QUERYSTRING = '?token=token123'
@@ -9,4 +11,10 @@ export const readEvents = () => async dispatch => {
   // 上記でconstを定義しているので、そこから呼び出しているその為、通常では''が必要だが変数として呼び出しているので''は必要ない
   const response = await axios.get(`${ROOT_URL}/events${QUERYSTRING}`)
   dispatch({type: READ_EVENTS, response})
+}
+
+export const postEvent = values => async dispatch => {
+  // 上記でconstを定義しているので、そこから呼び出しているその為、通常では''が必要だが変数として呼び出しているので''は必要ない
+  const response = await axios.post(`${ROOT_URL}/events${QUERYSTRING}`, values)
+  dispatch({type: CREATE_EVENT, response})
 }
