17934ab838d83210477262d593257e75780acf0d
takuyatechexpert <lp4546.sg.abc@gmail.com>
2020-03-19 13:32:28 +0900

    implement delete feature

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
 // リデューサーで再利用するらしい 再利用する場合はexportで記述する
 export const READ_EVENTS = 'READ_EVENTS'
+export const READ_EVENT = 'READ_EVENT'
 export const CREATE_EVENT = 'CREATE_EVENT'
-
+export const DELETE_EVENT = 'DELETE_EVENT'
+export const UPDATE_EVENT = 'UPDATE_EVENT'
 
 const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1'
 const QUERYSTRING = '?token=token123'
@@ -17,4 +19,22 @@ export const postEvent = values => async dispatch => {
   // 上記でconstを定義しているので、そこから呼び出しているその為、通常では''が必要だが変数として呼び出しているので''は必要ない
   const response = await axios.post(`${ROOT_URL}/events${QUERYSTRING}`, values)
   dispatch({type: CREATE_EVENT, response})
+}
+
+export const getEvent = id => async dispatch => {
+  const response = await axios.get(`${ROOT_URL}/events/${id}${QUERYSTRING}`)
+  dispatch({ type: READ_EVENT, response })
+}
+
+export const putEvent = values => async dispatch =>{
+  const response = await axios.put(`${ROOT_URL}/events/${values.id}${QUERYSTRING}`, values)
+  dispatch({ type: UPDATE_EVENT, response })
+
+}
+
+export const deleteEvent = id => async dispatch => {
+  // 上記でconstを定義しているので、そこから呼び出しているその為、通常では''が必要だが変数として呼び出しているので''は必要ない
+  await axios.delete(`${ROOT_URL}/events/${id}${QUERYSTRING}`)
+  dispatch({type: DELETE_EVENT, id})
+  // dispatchでリデューサーに渡している？
 }
--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,10 +1,22 @@
 import _ from 'lodash'
-import { READ_EVENTS } from '../actions'
+import { CREATE_EVENT, READ_EVENT, READ_EVENTS, DELETE_EVENT, UPDATE_EVENT } from '../actions'
 
 export default (events = {}, action) => {
-  switch (action.type){
+  switch (action.type) {
+    case CREATE_EVENT:
+    case READ_EVENT:
+    case UPDATE_EVENT:
+      const data = action.response.data
+      return { ...events, [data.id]: data }
     case READ_EVENTS:
       return _.mapKeys(action.response.data, 'id')
+    case DELETE_EVENT:
+      delete events[action.id]
+      // ここで送られてきたidのイベントを削除している
+      // これを記述しないとビューにリロードしないと反映されない
+      return { ...events }
+      // スプレット演算子? スプレッド構文らしいが厳密に名前はないらしい
+      // 配列の取り扱いが楽になり、関数もそのまま呼び出せるのだとか?
     default:
       return events
   }
